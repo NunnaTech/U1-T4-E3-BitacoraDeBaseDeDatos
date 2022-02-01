8ea0cfaaa4da35245e7c9eeaf5d94ac96f4e636a
unknown <sterplay2010@gmail.com>
2022-01-31 22:42:35 -0600

    Initial commit

---
+++ src/main/java/edu/utez/mx/citiutez/CitiutezApplication.java
@@ -0,0 +1,13 @@
+package edu.utez.mx.citiutez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiutezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiutezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utez/mx/citiutez/CitiutezApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utez.mx.citiutez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiutezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
