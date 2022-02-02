b69c04fc32919029ffecc3aa79dd43c3cf498f27
unknown <sterplay2010@gmail.com>
2022-02-02 11:58:11 -0600

    Pantallas hechas

--- src/main/java/edu/utez/mx/citiutez/CitiutezApplication.java
+++ src/main/java/edu/utez/mx/citiutez/CitiutezApplication.java
@@ -2,8 +2,10 @@ package edu.utez.mx.citiutez;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CitiutezApplication {
 
 	public static void main(String[] args) {
