1049e81f73fe79364937e9df979ef9feffcf1e9a
unknown <sterplay2010@gmail.com>
2022-02-03 12:34:20 -0600

    Bitacora de sesion hecha

--- src/main/java/edu/utez/mx/citiutez/controller/LoginController.java
+++ src/main/java/edu/utez/mx/citiutez/controller/LoginController.java
@@ -1,17 +1,25 @@
 package edu.utez.mx.citiutez.controller;
 
 import edu.utez.mx.citiutez.entity.Employee;
+import edu.utez.mx.citiutez.entity.SessionLogs;
 import edu.utez.mx.citiutez.service.EmployeeService;
+import edu.utez.mx.citiutez.service.SessionLogsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+
 
 @Controller
 public class LoginController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    @Autowired
+    private SessionLogsService sessionLogsService;
 
     @GetMapping("/")
     public String login(Model model){
@@ -22,9 +30,12 @@ public class LoginController {
     public String enter(@ModelAttribute(name="loginForm") Employee login){
         Employee session = employeeService.getByEmail(login.getEmail());
         if (session.isIsadmin()==true){
-            return "sessionLogs";
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            SessionLogs logs = new SessionLogs(timestamp,session);
+            sessionLogsService.save(logs);
+            return "redirect:/session";
         }else {
-            return "AllEmployee";
+            return "redirect:/employee/allEmployee";
         }
     }
 }
