9742868a4b6eba51ad622e3c507abcda23405cdd
unknown <sterplay2010@gmail.com>
2022-02-04 21:24:48 -0600

    Login Modificado

--- src/main/java/edu/utez/mx/citiutez/controller/LoginController.java
+++ src/main/java/edu/utez/mx/citiutez/controller/LoginController.java
@@ -1,15 +1,14 @@
 package edu.utez.mx.citiutez.controller;
 
 import edu.utez.mx.citiutez.entity.Employee;
-import edu.utez.mx.citiutez.entity.SessionLogs;
 import edu.utez.mx.citiutez.service.EmployeeService;
-import edu.utez.mx.citiutez.service.SessionLogsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 
 @Controller
@@ -18,27 +17,24 @@ public class LoginController {
     @Autowired
     private EmployeeService employeeService;
 
-    @Autowired
-    private SessionLogsService sessionLogsService;
-
     @GetMapping("/")
-    public String login(Model model){
+    public String login(Model model) {
         return "login";
     }
 
     @PostMapping("/login")
-    public String enter(@ModelAttribute(name="loginForm") Employee login){
-        Employee session = employeeService.getByEmail(login.getEmail());
-        if (session.isIsadmin()==true){
-            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-            SessionLogs logs = new SessionLogs(timestamp,session);
-            sessionLogsService.save(logs);
-            return "redirect:/session";
-        }else {
-            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-            SessionLogs logs = new SessionLogs(timestamp,session);
-            sessionLogsService.save(logs);
-            return "redirect:/employee/allEmployee";
+    public String enter(@ModelAttribute(name = "loginForm") Employee login, HttpSession sessionUser) {
+        Map<String, Object> session = employeeService.checkUser(login);
+        if (session.get("noUser") == "false") {
+            if (session.get("isAdmin") == "true") {
+                sessionUser.setAttribute("idUser", session.get("idUser"));
+                return "redirect:/session";
+            } else if (session.get("isAdmin") == "false") {
+                sessionUser.setAttribute("idUser", session.get("idUser"));
+                return "redirect:/employee/allEmployee";
+            }
         }
+        return "redirect:/";
     }
+
 }
